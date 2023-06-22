9e5622fbc18d1fd5c4b4df49fd3d75cce014f2b6
Sujit <mnamesujit@gmail.com>
2023-06-22 23:15:59 +0530

    added GET request that accept shortId and redirect to its original redirectURL page

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const { createURL } = require("../controller/urlController");
+const urlModel = require("../model/urlModel");
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).json({"message": "OK"}))
+// GET request
+router.get('/:shortId', async (req, res) => {
+    try{
+        const shortId = req.params.shortId;
+        const entry = await urlModel.findOneAndUpdate(
+            { shortId },
+            {
+                $push: {
+                    visitHistory: {
+                        timestamp: Date.now()
+                    }
+                }
+            }
+        )
+        res.redirect(entry.redirectURL)
+    }catch(err){
+        console.log(err)
+        res.status(400).json({
+            success: false,
+            messsage: err.message
+            })
+    }
+})
+
+// POST request
 router.post('/createurl', createURL)
-console.log("routes called")
 
 module.exports = router
